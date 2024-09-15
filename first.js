dea46c57ca0f0d10cf05b6a31bf752b2182eaf4e
Your Name <you@example.com>
2024-09-15 22:07:48 +0600

    making an tax calculator using js function

--- first.js
+++ first.js
@@ -1,1 +1,10 @@
-console.log(8)
+// problem-1
+function calculateTax(monthlyIncome,monthlyExpenses) {
+    if (monthlyIncome < 0 || monthlyExpenses < 0 ) {
+        console.log("Invalid Input")
+        return
+    }
+    const profit = ( monthlyIncome - monthlyExpenses );
+    const tax = ( profit * 0.2 );
+    return tax ;
+}
