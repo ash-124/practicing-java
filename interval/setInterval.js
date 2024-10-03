aa0b34291bb78b0bc2af536808cfe00c365b5721
Your Name <you@example.com>
2024-10-03 10:32:33 +0600

    done practicing setInterval()

--- interval/setInterval.js
+++ interval/setInterval.js
@@ -3,15 +3,19 @@ console.log("4");
 // syntex of setTimeout:-
 // setTimeout(function, time in ms)
 setTimeout(() => {
-    console.log(5);
-},2000)
+  console.log(5);
+}, 2000);
 // syntex of setInterval:-
 // setInterval(function, time-gap of repeatation)
 let num = 0;
-setInterval(() => {
-    
-    console.log(num = num + 1 );
-},4000)
+// setInterval(() => {
+
+//     console.log(num = num + 1 );
+// },4000)
+const clockId = setInterval(() => {
+  clearInterval(clockId);
+  console.log((num = num + 1));
+}, 4000);
 console.log("6");
 console.log("7");
 console.log("8");
